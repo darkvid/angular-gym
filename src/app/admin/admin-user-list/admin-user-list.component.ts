c625708bea450b006fbde167d09db0ea0b9719ed
David Galisteo <darkvidvsgalis@gmail.com>
2024-08-07 18:00:00 +0200

    creating user list component

---
+++ src/app/admin/admin-user-list/admin-user-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-user-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './admin-user-list.component.html',
+  styleUrl: './admin-user-list.component.css'
+})
+export class AdminUserListComponent {
+
+  users = ["David", "Carmen", "Manu", "Olga", "Pablo"]
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AdminUserListComponent } from './admin/admin-user-list/admin-user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'admin/userList',
+        component: AdminUserListComponent
+    }
+
+];
